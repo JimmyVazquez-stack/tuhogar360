654c075c4fb5af336a07c27f93b2d6ad300f4000
Jimmy <jv78676@gmail.com>
2024-03-26 21:50:10 -0600

    actualizacion del static

--- hogar360/settings.py
+++ hogar360/settings.py
@@ -123,11 +123,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'), # Agrega esta línea
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- hogar360/urls.py
+++ hogar360/urls.py
@@ -4,8 +4,8 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('usuarios.urls')),
     path('administrativos/', include('administrativos.urls')),
-    path('admin/', admin.site.urls),
     path('vista360/', include('vista360.urls')),
     path('catalogos/', include('catalogo.urls')),
     path('blog/', include('blog.urls')),
+    path('admin/', admin.site.urls),
 ]
